b7b423408e04fb953b0aa3af79f5cd051e6fcb74
Kizito Njoku <nkizito57@gmail.com>
2020-03-07 00:52:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const drvier = {
 };
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign( driver, key, value);
+  return Object.assign({}, driver, {[key]: value});
 }
