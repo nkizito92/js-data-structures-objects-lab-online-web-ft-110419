220e955a279ec5f8196d22b0c94c37d9ef3cf4fb
Kizito Njoku <nkizito57@gmail.com>
2020-03-07 00:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your solution in this file!
 
 
-const drvier = {}
+const drvier = {
+  name: "Kizito",
+  vehicle: "Honda",
+  type: "2door"
+};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   
